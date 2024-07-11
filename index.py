a20f6618d9188500872d8290734466f7d66ae325
K4 - Phong Do <dominhphong306@gmail.com>
2024-07-11 08:21:03 +0700

    feat: update index func, add translated content

--- index.py
+++ index.py
@@ -5,9 +5,11 @@ from llama_index.core import (
     StorageContext,
     load_index_from_storage,
 )
+from dotenv import load_dotenv
 
+load_dotenv()
 # NOTE: for local testing only, do NOT deploy with your key hardcoded
-os.environ["OPENAI_API_KEY"] = "your key here"
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 index = None
 
@@ -16,13 +18,20 @@ def initialize_index():
     global index
     index_dir = "./.index"
     
-    storage_context = StorageContext.from_defaults(persist_dir=index_dir)
+
     
     if os.path.exists(index_dir):
+        storage_context = StorageContext.from_defaults(persist_dir=index_dir)
         index = load_index_from_storage(storage_context)
     else:
+        storage_context = StorageContext.from_defaults()
         documents = SimpleDirectoryReader("./documents").load_data()
         index = VectorStoreIndex.from_documents(
             documents, storage_context=storage_context
         )
+
         storage_context.persist(index_dir)
+
+def get_index():
+    global index
+    return index
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from index import initialize_index
+from flask import request
+from index import initialize_index, get_index
 
 app = Flask(__name__)
 initialize_index()
@@ -9,6 +10,18 @@ initialize_index()
 def home():
     return "Hello World!"
 
+@app.route("/query", methods=["GET"])
+def query_index():
+    query_text = request.args.get("text", None)
+    if query_text is None:
+        return (
+            "No text found, please include a ?text=blah parameter in the URL",
+            400,
+        )
+    index = get_index()
+    query_engine = index.as_query_engine()
+    response = query_engine.query(query_text)
+    return str(response), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5601)
